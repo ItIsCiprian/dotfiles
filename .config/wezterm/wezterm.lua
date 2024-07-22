dd30ba98b1a13f40024b5055fbb34a08c49639d8
ItIsCiprian <ionutcipriananescu@gmail.com>
2024-07-22 22:59:34 +0300

    Update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,4 +1,4 @@
--- Pull in the wezterm API
+-- Pull in the WezTerm API
 local wezterm = require("wezterm")
 
 -- Create a configuration table
@@ -16,20 +16,20 @@ config.font_size = 12
 -- Disable the tab bar
 config.enable_tab_bar = false
 
--- Set window decorations to allow resizing
+-- Allow window resizing by setting window decorations
 config.window_decorations = "RESIZE"
 
 -- Set window background opacity
 config.window_background_opacity = 0.95
 
--- Enable background blur for macOS
+-- Enable background blur for macOS with a blur radius of 20
 config.macos_window_background_blur = 20
 
--- Ensure color scheme does not change when switching displays
+-- Ensure color scheme remains consistent when switching displays
 wezterm.on("window-config-reloaded", function(window, pane)
-	window:set_config_overrides({
-		color_scheme = "Catppuccin Macchiato",
-	})
+  window:set_config_overrides({
+    color_scheme = "Catppuccin Macchiato",
+  })
 end)
 
 -- Return the configuration to WezTerm
