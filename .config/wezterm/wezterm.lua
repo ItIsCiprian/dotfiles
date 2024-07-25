d04c8f1b9ee056b9664c54d18f544a0c91b4116d
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2024-07-25 23:32:10 +0300

    Stow file update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -10,7 +10,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Macchiato"
 
 -- Set the font and font size
-config.font = wezterm.font("MesloLGS NF")
+config.font = wezterm.font("MesloLGS Nerd Font")
 config.font_size = 12
 
 -- Disable the tab bar
@@ -20,16 +20,16 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
 -- Set window background opacity
-config.window_background_opacity = 0.95
+config.window_background_opacity = 80
 
 -- Enable background blur for macOS with a blur radius of 20
-config.macos_window_background_blur = 20
+config.macos_window_background_blur = 10
 
 -- Ensure color scheme remains consistent when switching displays
 wezterm.on("window-config-reloaded", function(window, pane)
-  window:set_config_overrides({
-    color_scheme = "Catppuccin Macchiato",
-  })
+	window:set_config_overrides({
+		color_scheme = "Catppuccin Macchiato",
+	})
 end)
 
 -- Return the configuration to WezTerm
