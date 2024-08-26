dd947fbdfe2f5407c87cd638c69731aecd24d9e9
Ionut Ciprian Anescu <ionutcipriananescu@gmail.com>
2024-08-26 23:17:43 +0300

    Stow file update

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,92 +1,46 @@
+
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-;; This is your private Doom Emacs configuration file. 
+;; This is your private Doom Emacs configuration file.
 ;; You do not need to run 'doom sync' after modifying this file.
 
 ;;------------------------------------------------------------------------------
 ;; User Information
 ;;------------------------------------------------------------------------------
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates, and snippets. This is optional.
+
 ;; (setq user-full-name "John Doe"
 ;;       user-mail-address "john@doe.com")
 
 ;;------------------------------------------------------------------------------
 ;; Font Configuration
 ;;------------------------------------------------------------------------------
-;; Doom exposes five (optional) variables for controlling fonts in Doom:
-;;
-;; - `doom-font' -- the primary font to use
-;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
-;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for presentations or streaming.
-;; - `doom-symbol-font' -- for symbols
-;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
-;;
-;; See 'C-h v doom-font' for documentation and more examples of what they accept. For example:
-;;
+
 ;; (setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-;; If you or Emacs can't find your font, use 'M-x describe-font' to look them up,
-;; `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to refresh your font settings.
-;; If Emacs still can't find your font, it likely wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;;------------------------------------------------------------------------------
 ;; Theme Configuration
 ;;------------------------------------------------------------------------------
-;; There are two ways to load a theme. Both assume the theme is installed and available.
-;; You can either set `doom-theme' or manually load a theme with the `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
 ;; Load the desired theme
-(setq doom-theme 'catppuccin-macchiato)
+(setq doom-theme 'doom-one) ;; Fallback theme in case of issues
 
-;; Ensure the Catppuccin theme package is loaded correctly
-(after! catppuccin-theme
+;; If you still want to use catppuccin-macchiato:
+;; Ensure the theme package is installed and properly loaded
+(use-package! catppuccin-theme
+  :config
+  (setq doom-theme 'catppuccin-macchiato) ;; Set the theme
   (setq catppuccin-flavor 'macchiato) ;; Select the flavor
   (catppuccin-reload)) ;; Reload the theme with the selected flavor
 
 ;;------------------------------------------------------------------------------
 ;; Line Numbers Configuration
 ;;------------------------------------------------------------------------------
-;; This determines the style of line numbers in effect. If set to `nil', line numbers are disabled.
-;; For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
 ;;------------------------------------------------------------------------------
 ;; Org-mode Configuration
 ;;------------------------------------------------------------------------------
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
 ;;------------------------------------------------------------------------------
 ;; Package Configuration
 ;;------------------------------------------------------------------------------
-;; Whenever you reconfigure a package, make sure to wrap your config in an `after!' block,
-;; otherwise Doom's defaults may override your settings. E.g.
-;;
-;;   (after! PACKAGE
-;;     (setq x y))
-;;
-;; The exceptions to this rule:
-;;
-;;   - Setting file/directory variables (like `org-directory')
-;;   - Setting variables which explicitly tell you to set them before their package is loaded (see 'C-h v VARIABLE' to look up their documentation).
-;;   - Setting doom variables (which start with 'doom-' or '+').
-
-;;------------------------------------------------------------------------------
-;; Useful Functions/Macros for Configuring Doom
-;;------------------------------------------------------------------------------
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to this file.
-;;   Emacs searches the `load-path' when you load packages with `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol and press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces, etc).
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how they are implemented.
